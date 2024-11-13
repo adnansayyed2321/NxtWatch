0ca3a9947d15037b3788e482e43894d5641a5388
Adnan Sayyed <adnansayyed2321@gmail.com>
2024-11-13 15:36:14 +0530

    like dislike

--- src/components/DetailsVideo/DetailVideo.js
+++ src/components/DetailsVideo/DetailVideo.js
@@ -5,12 +5,39 @@ import Cookies from "js-cookie"
 import { DETAIL_VIDEO_URL } from "../../utils/constants"
 import Shimmer from "../Shimmer/Shimmer"
 import ReactPlayer from 'react-player'
+import { TbPointFilled } from "react-icons/tb";
+import { GrLike } from "react-icons/gr";
+import { GrDislike } from "react-icons/gr";
+import { MdOutlinePlaylistAdd } from "react-icons/md";
+
 
 const DetailVideo = () => {
 
     const [videoDetails,setVideoDetails] = useState({})
+    const [like,setLike] = useState(false)
+    const [dislike,setDislike] = useState(false)
     const {id} = useParams()
     const token = Cookies.get("jwt_token")
+    const publishedAt = new Date(videoDetails.published_at).getFullYear()
+    const currentYear = new Date().getFullYear()
+
+    const handelLike = () => {
+        if(like){
+            setLike(false)
+        }else{
+            setLike(true)
+            setDislike(false)
+        }
+    }
+
+    const handelDisLike = () => {
+        if (dislike){
+            setDislike(false)
+        }else{
+            setDislike(true)
+            setLike(false)
+        }
+    }
 
     const getVideoDetails = useCallback ( async () => {
         const options = {
@@ -43,12 +70,30 @@ const DetailVideo = () => {
     return (
         <div className="flex w-full ">
         <Sidebar/>
-        <div className="pt-16 p-9 w-4/5  overflow-y-auto h-screen">
+        <div className="pt-18 p-9 w-4/5  overflow-y-auto h-screen">
         {
             Object.keys(videoDetails).length === 0 ? <Shimmer/> :
             <div className="flex flex-col gap-5"> 
-                <div className="pt-10  w-full">
-                    <ReactPlayer width="80%" height ="520px" url={videoDetails.video_url} controls/> 
+                <div className="pt-12 w-full ">
+                    <ReactPlayer className="bg-gray-300 rounded-md" width="80%" height ="520px" url={videoDetails.video_url} controls/> 
+                </div>
+                <h1 className="text-xl font-medium">{videoDetails.title}</h1>
+                <div className="flex items-center justify-between mr-9">
+                    <p className="text-gray-500 flex items-center gap-1">{videoDetails.view_count} views <TbPointFilled className="text-gray-500"/> {currentYear-publishedAt} years ago</p>
+                    <div className="flex items-center gap-10">
+                        <button onClick={handelLike} className = {`flex items-center gap-2 ${like && "text-blue-600" } `}><GrLike /> Like</button>
+                        <button onClick={handelDisLike} className = {`flex items-center gap-2 ${dislike && "text-blue-600" } `}><GrDislike /> Dislike</button>
+                        <button className="flex items-center gap-2"><MdOutlinePlaylistAdd className="text-2xl" />Save</button>
+                    </div>
+                </div>
+                <hr className="bg-gray-400 h-1 border-0"/>
+                <div className="flex items-start gap-3">
+                    <img className="w-14" src ={videoDetails.channel.profile_image_url} alt ="profile"/>
+                    <div className="flex flex-col">
+                        <p className="font-medium">{videoDetails.channel.name}</p>
+                        <p className="text-sm">{videoDetails.channel.subscriber_count} subscribers</p>
+                        <p className="pt-4">{videoDetails.description}</p>
+                    </div>
                 </div>
             </div>
         }
--- src/components/VideoComponent/VideoComponent.js
+++ src/components/VideoComponent/VideoComponent.js
@@ -8,7 +8,7 @@ const VideoComponent = ({videoDetails}) => {
 
     return (
         <div className="  w-full flex flex-col gap-3">
-            <img className="w-full h-48" src = {thumbnail_url} alt="thumbnail"/>
+            <img className="w-full h-48 rounded-md" src = {thumbnail_url} alt="thumbnail"/>
             <div className="flex gap-3 items-start">
                 <img className="rounded-s-full w-10" src ={channel.profile_image_url} alt="channel-pro"/>
                 <div className="flex flex-col gap-1">
